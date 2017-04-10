40a08a26eb9461e643824a86c593d715e2976238
Andrew <0x384c0@gmail.com>
2017-04-10 16:10:48 +0600

    initial commit

---
+++ Experiments-iOS-XMPP/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  Experiments-iOS-XMPP
+//
+//  Created by 0x384c0 on 10.04.17.
+//  Copyright © 2017 0x384c0. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
---
+++ Experiments-iOS-XMPP/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Experiments-iOS-XMPP
+//
+//  Created by 0x384c0 on 10.04.17.
+//  Copyright © 2017 0x384c0. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+    }
+}
+
