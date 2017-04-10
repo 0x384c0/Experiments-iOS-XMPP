d803c516aaea5a62079b12edf2d7a4aad7797df9
Andrew <0x384c0@gmail.com>
2017-04-10 17:29:59 +0600

    added XMPPWraper

--- Experiments-iOS-XMPP/ViewController.swift
+++ Experiments-iOS-XMPP/ViewController.swift
@@ -9,7 +9,58 @@
 import UIKit
 
 class ViewController: UIViewController {
-    override func viewDidLoad() {
+    
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passTextField: UITextField!
+    @IBOutlet weak var buggiesListLabel: UILabel!
+    
+    @IBAction func connectTap(_ sender: UIButton) {
+        dismissKB()
+        buggiesListLabel.text = ""
+        if
+            let login = loginTextField.text,
+            login != "",
+            let pass = passTextField.text,
+            pass != "" {
+            xmppWrapper = XMPPWraper(jabberID: login, userPassword: pass)
+            xmppWrapper?.delegate = self
+            DispatchQueue.global().async {
+                print("Connecting...")
+                let result = self.xmppWrapper?.connect()
+                print("Connected: \(result)")
+            }
+        }
+    }
+    @IBAction func disconnectTap(_ sender: UIButton) {
+        dismissKB()
+        xmppWrapper?.disconnect()
+    }
+    
+    @IBAction func fetchBuddies(_ sender: UIButton){//contacts
+        self.title = xmppWrapper?.xmppStream.myJID.bare()
+        
+        xmppWrapper?.xmppRoster.fetch()
+    }
+    
+    var xmppWrapper:XMPPWraper?
+    
+    func dismissKB(){
+        loginTextField.resignFirstResponder()
+        passTextField.resignFirstResponder()
     }
 }
 
+extension ViewController : ChatDelegate{
+    func buddyWentOnline(_ name: String){
+        print(#function)
+        print(name)
+        buggiesListLabel.text?.append("\(name)\n")
+    }
+    func buddyWentOffline(_ name: String){
+        print(#function)
+        print(name)
+    }
+    func didDisconnect(){
+        print(#function)
+    }
+}
---
+++ Experiments-iOS-XMPP/XMPPWraper.swift
@@ -0,0 +1,130 @@
+//
+//  XMPPWraper.swift
+//  Experiments-iOS-XMPP
+//
+//  Created by 0x384c0 on 10.04.17.
+//  Copyright © 2017 0x384c0. All rights reserved.
+//
+
+import XMPPFramework
+
+
+class XMPPWraper : NSObject, XMPPRosterDelegate, XMPPStreamDelegate{
+    let
+    xmppStream = XMPPStream()!,
+    xmppRosterStorage = XMPPRosterCoreDataStorage(),
+    jabberID:String,
+    userPassword:String
+    var
+    delegate:ChatDelegate! = nil,
+    xmppRoster: XMPPRoster
+    init(jabberID:String, userPassword:String) {
+        self.jabberID = jabberID
+        self.userPassword = userPassword
+        xmppRoster = XMPPRoster(rosterStorage: xmppRosterStorage)
+        DDLog.add(DDTTYLogger.sharedInstance)
+        super.init()
+        setupStream()
+    }
+    
+    //MARK: Private Methods
+    private func setupStream() {
+        //xmppRoster = XMPPRoster(rosterStorage: xmppRosterStorage)
+        xmppRoster.activate(xmppStream)
+        xmppStream.addDelegate(self, delegateQueue: DispatchQueue.main)
+        xmppRoster.addDelegate(self, delegateQueue: DispatchQueue.main)
+    }
+    private func goOnline() {
+        let presence = XMPPPresence()
+        let domain = xmppStream.myJID.domain
+        
+        if domain == "gmail.com" || domain == "gtalk.com" || domain == "talk.google.com" {
+            let priority = DDXMLElement.element(withName: "priority", stringValue: "24") as! DDXMLElement
+            presence?.addChild(priority)
+        }
+        xmppStream.send(presence)
+    }
+    private func goOffline() {
+        let presence = XMPPPresence(type: "unavailable")
+        xmppStream.send(presence)
+    }
+    
+    func connect() -> Bool {
+        if !xmppStream.isConnected() {
+            
+            if !xmppStream.isDisconnected() {
+                return true
+            }
+            xmppStream.myJID = XMPPJID(string: jabberID)
+            
+            do {
+                try xmppStream.connect(withTimeout: XMPPStreamTimeoutNone)
+                print("Connection success")
+                return true
+            } catch {
+                print("Something went wrong!")
+                return false
+            }
+        } else {
+            return true
+        }
+    }
+    func disconnect() {
+        goOffline()
+        xmppStream.disconnect()
+    }
+    
+    //MARK: XMPP Delegates
+    //XMPPStreamDelegate
+    func xmppStreamDidConnect(_ sender: XMPPStream!) {
+        do {
+            try	xmppStream.authenticate(withPassword: userPassword)
+        } catch {
+            print("Could not authenticate")
+        }
+    }
+    func xmppStreamDidAuthenticate(_ sender: XMPPStream!) {
+        goOnline()
+    }
+    func xmppStreamDidChangeMyJID(_ xmppStream: XMPPStream!) {
+        print(#function)
+        print(xmppStream)
+    }
+    //XMPPRosterDelegate
+    
+    
+    func xmppStream(_ sender: XMPPStream!, didReceive iq: XMPPIQ!) -> Bool {
+        print("Did receive IQ")
+        return false
+    }
+    func xmppStream(_ sender: XMPPStream!, didReceive message: XMPPMessage!) {
+        print("Did receive message \(message)")
+    }
+    func xmppStream(_ sender: XMPPStream!, didSend message: XMPPMessage!) {
+        print("Did send message \(message)")
+    }
+    func xmppStream(_ sender: XMPPStream!, didReceive presence: XMPPPresence!) {
+        let presenceType = presence.type()
+        let myUsername = sender.myJID.user
+        let presenceFromUser = presence.from().user
+        
+        if presenceFromUser != myUsername {
+            print("Did receive presence from \(presenceFromUser)")
+            if presenceType == "available" {
+                delegate.buddyWentOnline("\(presenceFromUser)@gmail.com")
+            } else if presenceType == "unavailable" {
+                delegate.buddyWentOffline("\(presenceFromUser)@gmail.com")
+            }
+        }
+    }
+    func xmppRoster(_ sender: XMPPRoster!, didReceiveRosterItem item: DDXMLElement!) {
+        print("Did receive Roster item")
+    }
+}
+
+protocol ChatDelegate {
+    func buddyWentOnline(_ name: String)
+    func buddyWentOffline(_ name: String)
+    func didDisconnect()
+}
+
